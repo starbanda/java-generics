99c55ba0e845f855d25b7f4ad17d56fc27cd444c
VipulArora_2003 <vipularora9930@gmail.com>
2025-04-04 11:38:15 +0530

    [Vipul] Add: Defined Product class with attributes and operations

---
+++ src/com/bridgelabz/dynamicOnlineMarketplace/Product.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.dynamicOnlineMarketplace;
+
+class Product<T extends Category> {
+    private String name;
+    private double price;
+    private T category;
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product: " + name + ", Price: " + price + ", Category: " + category.getCategoryName();
+    }
+}
